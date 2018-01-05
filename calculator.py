495cb3ea99d42f691afd888ed3d1349f60e36fa0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 17:25:24 -0800

    Updated to pass lists

--- calculator.py
+++ calculator.py
@@ -6,31 +6,43 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-def calculator():
-    should_continue = True
-    while should_continue:
-        equation = raw_input("What do you want to calculate? (in prefix notation)")
-        tokens = equation.split(' ')
-        for num in range(1, len(tokens)):
-            tokens[num] = int(tokens[num])
-        print tokens
-        if tokens[0] == 'q':
-            should_continue = False
-        elif tokens[0] == '+':
-            print add(tokens[1], tokens[2])
-        elif tokens[0] == '-':
-            print subtract(tokens[1], tokens[2])
-        elif tokens[0] == '*':
-            print multiply(tokens[1], tokens[2])
-        elif tokens[0] == '/':
-            print divide(tokens[1], tokens[2])
-        elif tokens[0] == 'square':
-            print square(tokens[1])
-        elif tokens[0] == "cube":
-            print cube(tokens[1])
-        elif tokens[0] == 'pow':
-            print power(tokens[1], tokens[2])
-        elif tokens[0] == 'mod':
-            print mod(tokens[1], tokens[2])
 
-calculator()
+
+while True:
+    equation = raw_input("What do you want to calculate? (in prefix notation)")
+    tokens = equation.split()
+    
+    operator = tokens[0]
+    numbers = []
+
+    for token in tokens[1:]:
+        try:
+            numbers.append(int(token))
+        except:
+            print "Please only use operators and numbers."
+            break
+    
+    # print tokens
+    if len(numbers) > 0:
+        if operator == 'q':
+            break
+        elif operator == '+':
+            print add(numbers)
+        elif operator == '-':
+            print subtract(numbers)
+        elif operator == '*':
+            print multiply(numbers)
+        elif operator == '/':
+            print divide(numbers)
+        elif operator == 'square':
+            print square(numbers)
+        elif operator == "cube":
+            print cube(numbers)
+        elif operator == 'pow':
+            print power(numbers)
+        elif operator == 'mod':
+            print mod(numbers)
+        else:
+            print 'The request was not valid.'
+
+
